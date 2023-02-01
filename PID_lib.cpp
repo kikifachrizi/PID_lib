2a178535ceec6f4ae8c2ce5864a5263390e2c010
Rifqy Fachrizi <kikifachrizi@gmail.com>
2023-02-01 14:31:27 +0700

    update pid_speed

--- PID_lib.cpp
+++ PID_lib.cpp
@@ -7,13 +7,13 @@ PID_lib::PID_lib(PinName direksi1, PinName direksi2, PinName pulseWidth) : dir1(
     pwm = 0;
     
     ppr = 3600.0f;
-    float phi = 3.14285714;
+    double phi = 3.14285714;
     
 //    t.start();
 //    tr.reset();
 }
 
-void PID_lib::manualPwm(float speed_){
+void PID_lib::manualPwm(double speed_){
     if(speed_ < 0){
     //   pwm.write(speed_);
       dir1 = 0;
@@ -29,10 +29,10 @@ void PID_lib::manualPwm(float speed_){
 }
 
 void PID_lib::stop(){
-    rpm = 0;
-    rpmn1 = 0;
-    rpmFilt = 0;
-    rpmFiltn1 = 0;
+    rps = 0;
+    rpsn1 = 0;
+    rpsFilt = 0;
+    rpsFiltn1 = 0;
     lastPid = 0;
     e = 0;
     eI = 0;
@@ -43,63 +43,15 @@ void PID_lib::stop(){
     dir2 = 0;
 }
 
-// kp ki kd untuk sp 50 tuning manual lagi stelah tuning matlab
-void PID_lib::pwm_read(float target_, float kp_,float ki_,float kd_, float freq, float t_){
-    tim = t_;
-    dt = tim - lastime;
-        
-    rpm = freq/ppr*60;
-        
-    rpmFilt = 0.03046875*rpm + 0.03046875*rpmn1 + 0.93906251*rpmFiltn1;//10hz filter 
-
-      //error computing start          
-    e = target_ - rpmFilt;
-    eI += e;
-    eD = e - laste;
-      //error computing end
-      
-      //storing error value start
-    hP = e*kp_;
-    hI = eI*ki_*dt;
-    hD = eD*kd_/dt;
-      //storing error value end
-
-    //saturasi ki start
-    if(hI < 0.5 && hI > 0){
-        setI = hI;
-    }else if(hI > 0.5){
-        setI = 0.5;
-    }
-    else if(hI < 0 && hI > -0.5){
-        setI = hI;
-    }else if(hI < -0.5){
-        setI = -0.5;
-    }
-    //saturasi ki end
-    
-    pidPwm = hP+setI+hD;//pwm pid
-    lastime = tim;//update timer
-    laste = e;//update error
-      
-    rpmFiltn1 = rpmFilt;rpmn1 = rpm;//update filter
-    lastPid = pidPwm;
-//    printf("%f;%f;%f\n",e,eI,eD);
-    printf("%f;%f;%f\n", hP,setI,hD);
-    
-}
-
-
-
-void PID_lib::pid_pwm(float target_, float kp_,float ki_,float kd_, float freq, float t_){
+void PID_lib::pid_speed(double target_, double kp_,double ki_,double kd_, double rps, double t_){
   tim = t_;
   dt = tim - lastime;
   
-  rpm = freq/ppr*60;
-
-  rpmFilt = 0.03046875*rpm + 0.03046875*rpmn1 + 0.93906251*rpmFiltn1;//10hz filter 
+//   rpm = freq/ppr*60;
+  rpsFilt = 0.03046875*rps + 0.03046875*rpsn1 + 0.93906251*rpsFiltn1;//10hz filter 
 
   //error computing start          
-  e = target_ - rpmFilt;
+  e = target_ - rps;
   eI += e;
   eD = e - laste;
   //error computing end
@@ -110,7 +62,7 @@ void PID_lib::pid_pwm(float target_, float kp_,float ki_,float kd_, float freq, 
   hD = eD*kd_/dt;
   //storing error value end
   
-  //saturasu ki start
+  //saturasi ki start
   if(hI < 0.5 && hI > 0){
     setI = hI;
   }else if(hI > 0.5){
@@ -127,20 +79,20 @@ void PID_lib::pid_pwm(float target_, float kp_,float ki_,float kd_, float freq, 
   lastime = tim;//update timer
   laste = e;//update error
   
-  rpmFiltn1 = rpmFilt;rpmn1 = rpm;//update filter
+  rpsFiltn1 = rpsFilt;rpsn1 = rps;//update filter
   //motor direksi start
   if(pidPwm < 0){dir1 = 1; dir2 = 0;}
   else{dir1 = 0; dir2 = 1;}
   //motor direksi end
   
   //pwm saturasi start
-  if(pidPwm > 1){
-     pwmLebih = 1;
-  }else if(pidPwm < 1 && pidPwm > 0){
+  if(pidPwm > 0.5){
+     pwmLebih = 0.5;
+  }else if(pidPwm < 0.5 && pidPwm > 0){
      pwmLebih = fabs(pidPwm);
-  }else if(pidPwm < -1){
-     pwmLebih = -1;
-  }else if(pidPwm > -1 && pidPwm < 0){
+  }else if(pidPwm < -0.5){
+     pwmLebih = -0.5;
+  }else if(pidPwm > -0.5 && pidPwm < 0){
      pwmLebih = fabs(pidPwm);
   }
   //pwm saturasi end
@@ -152,32 +104,17 @@ void PID_lib::pid_pwm(float target_, float kp_,float ki_,float kd_, float freq, 
   lastPid = pidPwm;
 //  printf("%f\n", rpmFilt);
 //    printf("%f;%f;%f\n",e,eI,eD);
-    printf("%f;%f\n", rpm, rpmFilt);
+    printf("%f;%f\n", rps, rpsFilt);
 //    printf("%f;%f;%f\n",hP,setI,hD);
 //  pc.printf("%f;%1lu\n",pidPwm,t1.read_high_resolution_us());
 }
 
-// float PID_lib::record_data(int dir_,float speed_, float rpm_rec_, float t_r){
-//       rpm_rec = rpm_rec_;
-//     //   rpmFilt = 0.03046875*rpm_rec + 0.03046875*rpmn1 + 0.93906251*rpmFiltn1;//10 hz
-//     //   rpmFilt = 0.04503501*rpm_rec + 0.04503501*rpmn1 + 0.90992999*rpmFiltn1;//15 hz
-// //   rpmFilt = 0.0591907*rpm_rec + 0.0591907*rpmn1 + 0.88161859*rpmFiltn1;//20 hz
+
+//       rpmFilt = 0.03046875*rpm_rec + 0.03046875*rpmn1 + 0.93906251*rpmFiltn1;//10 hz
+//       rpmFilt = 0.04503501*rpm_rec + 0.04503501*rpmn1 + 0.90992999*rpmFiltn1;//15 hz
+//       rpmFilt = 0.0591907*rpm_rec + 0.0591907*rpmn1 + 0.88161859*rpmFiltn1;//20 hz
 //       rpmFilt = 0.07295966*rpm_rec_ + 0.07295966*rpmn1 + 0.85408069*rpmFiltn1;//25 hz
-//     //   rpmFilt = 0.09942446*rpm_rec + 0.09942446*rpmn1 + 0.80115107*rpmFiltn1;//35 hz
-//     //   rpmFilt = 0.13672874*rpm_rec + 0.13672874*rpmn1 + 0.72654253*rpmFiltn1;//50 hz
+//       rpmFilt = 0.09942446*rpm_rec + 0.09942446*rpmn1 + 0.80115107*rpmFiltn1;//35 hz
+//       rpmFilt = 0.13672874*rpm_rec + 0.13672874*rpmn1 + 0.72654253*rpmFiltn1;//50 hz
       
-//        if(dir_ == 1){
-//          pwm = speed_;
-//          dir1 = 0;
-//          dir2 = 1;
-//        }else if(dir_ == 0){
-//          pwm = speed_;
-//          dir1 = 1;
-//          dir2 = 0;
-//        }
-        
-//     rpmn1 = rpm_rec;rpmFiltn1 = rpmFilt;
-//     return rpmFilt;
-//     //    printf("%f;%.2f;%llu\n",speed_,rpm_rec,t_r);
-// }
 
